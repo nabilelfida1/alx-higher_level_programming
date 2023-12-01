9b1e53a9edf9c1eebf1e7b23eed6b70b44a3e8e4
Nabilelfida1 <Mrnabilelfida@gmai.com>
2023-11-30 16:23:51 -0800

    modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
-
         for i in range(4, 6):
             c = add(c, i)
+        return (c)
 
-        return c
-
-    return sub(a, b)
+    else:
+        return(sub(a, b))
--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-import fast_alphabet_103
+import string
+print(string.ascii_uppercase)#!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
