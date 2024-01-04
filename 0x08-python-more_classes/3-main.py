ea233e095144160cfe0c715445a7a0912de4b9d6
Nabilelfida1 <Mrnabilelfida@gmai.com>
2024-01-04 10:50:37 -0800

    task 0x08

---
+++ 0x08-python-more_classes/3-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(4, 2)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 3
+my_rectangle.height = 10
+print(my_rectangle)
+print(repr(my_rectangle))
+
